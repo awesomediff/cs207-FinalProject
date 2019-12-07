a1d23a8fa3a52527bb80ebc65c1dc3bc5ff9d448
Qianxue Yang <yqx153@gmail.com>
2019-12-07 01:55:49 -0500

    add Newtons method to init

--- awesomediff/__init__.py
+++ awesomediff/__init__.py
@@ -9,4 +9,5 @@ from awesomediff.func import exp
 from awesomediff.func import sinh
 from awesomediff.func import cosh
 from awesomediff.func import tanh
-__all__ = ['variable', 'evaluate', 'sin', 'cos', 'tan','log','sqrt','exp']
+from awesomediff.solvers import uni_Newton
+__all__ = ['variable', 'evaluate', 'sin', 'cos', 'tan','log','sqrt','exp','uni_Newton']
