f77d45f88836137df712dd7a6475c308a7ce235f
Gabriel <gpestre@users.noreply.github.com>
2019-12-09 11:35:20 -0500

    Clean up __init__.py import lists.

--- awesomediff/__init__.py
+++ awesomediff/__init__.py
@@ -3,15 +3,32 @@ from awesomediff.core import evaluate
 from awesomediff.func import sin
 from awesomediff.func import cos
 from awesomediff.func import tan
-from awesomediff.func import log
-from awesomediff.func import sqrt
-from awesomediff.func import exp
-from awesomediff.func import sinh
-from awesomediff.func import cosh
-from awesomediff.func import tanh
-from awesomediff.solvers import uni_Newton
-from awesomediff.func import logistic
 from awesomediff.func import arcsin
 from awesomediff.func import arccos
 from awesomediff.func import arctan
-__all__ = ['variable', 'evaluate', 'sin', 'cos', 'tan','log','sqrt','exp','uni_Newton','logistic','arcsin','arccos','arctan']
+from awesomediff.func import sinh
+from awesomediff.func import cosh
+from awesomediff.func import tanh
+from awesomediff.func import log
+from awesomediff.func import sqrt
+from awesomediff.func import exp
+from awesomediff.func import logistic
+from awesomediff.solvers import uni_Newton
+__all__ = [
+    'variable',
+    'evaluate',
+    'sin',
+    'cos',
+    'tan',
+    'arcsin',
+    'arccos',
+    'arctan',
+    'sinh',
+    'cosh',
+    'tanh',
+    'log',
+    'sqrt',
+    'exp',
+    'logistic',
+    'uni_Newton',
+]
