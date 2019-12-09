5ad2c63ded1f811110dbe4c9ea5a9b63dccd4616
Gabriel <gpestre@users.noreply.github.com>
2019-12-09 15:40:17 -0500

    Update solvers.

--- awesomediff/solvers.py
+++ awesomediff/solvers.py
@@ -1,5 +1,6 @@
 from inspect import signature
 
+import random
 
 from awesomediff.core import variable
 from awesomediff.core import evaluate
@@ -15,58 +16,129 @@ from awesomediff.func import cosh
 from awesomediff.func import tanh
 
 
+def l1_norm(vals):
+    return sum([abs(v) for v in vals])
+
+def l2_norm(vals):
+    return sqrt(sum([v**2 for v in vals])).val
+
+def mean(vals):
+    return sum([v for v in vals])/len(vals)
+
+def variance(vals):
+    mu = mean(vals)
+    return sum([(v-mu)**2 for v in vals])/len(vals)
+ 
 def mean_squared_error(y_true,y_pred):
-
+ 
     assert len(y_true)==len(y_pred)
+    return sum([(true-pred)**2 for true,pred in zip(y_true,y_pred)]) / len(y_true)
+ 
+def sum_square_residuals(y_true,y_pred):
+ 
+    assert len(y_true)==len(y_pred)
+    return sum([(true-pred)**2 for true,pred in zip(y_true,y_pred)])
+ 
+def total_sum_squares(y_true):
+ 
+    y_bar = mean(y_true)
+    return sum([(true-y_bar)**2 for true in y_true])
+ 
+def r2_score(y_true,y_pred):
+ 
+    assert len(y_true)==len(y_pred)
+    rss = sum_square_residuals(y_true,y_pred)
+    tss = total_sum_squares(y_true)
+    return 1 - rss/tss
 
-    loss = 0
-    for true,pred in zip(y_true,y_pred):
-        loss += (true-pred)**2
-    loss = loss / len(y_true)
-    return loss
+def transpose(M,check=True):
+    """Transpose a matrix (list of lists)."""
+
+    if check:
+        M = _check_inputs(M)
+    
+    n_rows = len(M)
+    n_cols = len(M[0])
+    new_M = []
+
+    for c in range(n_cols):
+        new_row = []
+        for r in range(n_rows):
+            new_row.append( M[r][c] )
+        new_M.append( new_row )
+    
+    return new_M
 
 
-def _check_inputs(X,y):
+def standardize(M,check=True,return_stats=False):
+    """
+        Transform a matrix (list of lists) so that each column has mean 0 and standard deviation 1.
+        If `return_stats==True`, returns a tuple: transposed_matrix, list_of_means, list_of_standard_deviations.
+        If `check==True`, ensures that the input is a list of lists.
+    """
+
+    if check:
+        M = _check_inputs(M)
+    M_ = []
+    Mt = transpose(M,check=False)
+    mus = []
+    sigmas = []
+    for row in Mt:
+        mu = mean(row)
+        sigma = sqrt(variance(row)).val
+        M_.append( [(v-mu)/sigma for v in row] )
+        mus.append(mu)
+        sigmas.append(sigma)
+    M_ = transpose(M_,check=False)
+    if return_stats:
+        return (M_,mus,sigmas)
+    else:
+        return M_
+
+
+def _check_inputs(X,y=None):
     """Converts a matrix X into a list of lists and a vector y into a list."""
 
-    new_y = []
     new_X = []
-
-    for val in y:
-        try:
-            new_y.append( float(val) )
-        except:
-            new_y.append( float(val[0]) )
-
-    assert len(X)==len(y), "Dimensions of X and y do not match."
-
+    
     for row in X:
         new_row = []
         for val in row:
             new_row.append( float(val) )
         new_X.append( new_row )
 
-    return new_X,new_y
+    if y is None:
+
+        return new_X
+
+    else:
+        
+        new_y = []
+
+        for val in y:
+            try:
+                new_y.append( float(val) )
+            except:
+                new_y.append( float(val[0]) )
+
+        assert len(X)==len(y), "Dimensions of X and y do not match."
+
+        return new_X,new_y
 
 
 class Solver:
 
-    def __init__(self):
+    def __init__(self,model,**solver_params):
+        self.model = model
         raise NotImplementedError
 
-    def reset(self):
-        raise NotImplementedError
-
-    def step(self):
-        raise NotImplementedError
-
-    def solve(self):
+    def solve(self,model_params,initial_weights,X,y):
         raise NotImplementedError
 
 
 class GradientDescent(Solver):
     
-    def __init__(self,model,learning_rate=0.01,rel_tol=1e-3,abs_tol=1e-6,max_iter=1000,random_seed=None,verbose=False):
+    def __init__(self,model,learning_rate=0.01,rel_tol=1e-5,abs_tol=1e-8,max_iter=10000,random_seed=None,verbose=False):
         
         self.model = model
         self.learning_rate = learning_rate
@@ -76,7 +148,7 @@ class GradientDescent(Solver):
         self.random_seed = random_seed
         self.verbose = verbose
 
-    def solve(self,initial_weights,X,y):
+    def solve(self,model_params,initial_weights,X,y):
 
         # Set random seed:
         if self.random_seed is not None:
@@ -85,7 +157,7 @@ class GradientDescent(Solver):
             random.seed()
         alpha = self.learning_rate
         def loss_func(*weights):
-            return self.model._loss(weights,X,y)
+            return self.model._loss(model_params,weights,X,y)
             
         prev_loss = None
         prev_weights = initial_weights
@@ -102,12 +174,18 @@ class GradientDescent(Solver):
             # Check for stopping conditions:
             if iteration >= self.max_iter:
                 converged = True
+                if self.verbose:
+                    print("Stop : iteration={}".format(iteration))
                 break
             if (prev_loss is not None) and (abs(loss-prev_loss) < self.abs_tol):
                 converged = True
+                if self.verbose:
+                    print("Stop : abs_tol={}".format(abs(loss-prev_loss)))
                 break
             if (prev_loss is not None) and (abs((loss-prev_loss)/prev_loss) < self.rel_tol):
                 converged = True
+                if self.verbose:
+                    print("Stop : rel_tol={}".format(abs((loss-prev_loss)/prev_loss)))
                 break
             prev_loss = loss
             prev_weights = weights
@@ -127,20 +205,24 @@ class Model:
         """Initialize the model."""
         pass
 
-    def _pack_weights(self,placeholder):
+    @classmethod
+    def _pack_weights(cls,model_params,placeholder):
         """Create a single list of weights to pass to the solver."""
         raise NotImplementedError
 
-    def _unpack_weights(self,weights):
-        """Extract weights from list returned by solver."""
+    @classmethod
+    def _unpack_weights(cls,model_params,weights):
+        """Extract weights from list returned by the solver."""
         raise NotImplementedError
 
-    def _loss(self,weights,X,y):
-        """Calculate the loss between y and the predictions made with X."""
+    @classmethod
+    def _loss(cls,model_params,weights,X,y):
+        """Calculate the loss between y and the predictions made with X using specified parameters and weights."""
         raise NotImplementedError
 
-    def _predict(self,weights,X):
-        """Calculates the """
+    @classmethod
+    def _predict(cls,model_params,weights,X):
+        """Predict X using specified parameters and weights."""
         raise NotImplementedError
 
     def predict(self,X):
@@ -159,6 +241,7 @@ class Model:
 class LinearRegression(Model):
 
     def __init__(self,fit_intercept=True,solver='gradient_descent',**solver_kwargs):
+        """Initialize the model."""
 
         valid_solvers = ['gradient_descent']
         if solver=='gradient_descent':
@@ -166,40 +249,49 @@ class LinearRegression(Model):
         else:
             raise ValueError("Solver must be one of the following: {}".format(", ".join(valid_solvers)))
         
+        self.standardize = False  # Ensure that features have mean zero and standrd deviation one before solving.
         self.fit_intercept = fit_intercept
         self.intercept = None
         self.coefs = []
 
-    def _pack_weights(self,intercept,coefs):
-        """Creates a single list of weights to pass to solver."""
+    @classmethod
+    def _pack_weights(cls,model_params,intercept,coefs):
+        """Create a single list of weights to pass to the solver."""
 
-        if self.fit_intercept:
+        if model_params['fit_intercept']:
             return [intercept]+coefs
         else:
             return coefs
 
-    def _unpack_weights(self,weights):
-        """Extracts relevant parameters to pass to solver"""
+    @classmethod
+    def _unpack_weights(cls,model_params,weights):
+        """Extract weights from list returned by the solver."""
 
-        if (self.fit_intercept==True) and len(weights)==1:
+        fit_intercept = model_params['fit_intercept']
+
+        if (fit_intercept==True) and len(weights)==1:
             intercept = weights[0]
             coefs = []
-        elif (self.fit_intercept==True) and len(weights)>1:
+        elif (fit_intercept==True) and len(weights)>1:
             intercept = weights[0]
             coefs = weights[1:]
-        elif (self.fit_intercept==False):
+        elif (fit_intercept==False):
             intercept = 0
             coefs = weights
         
         return intercept,coefs
 
-    def _loss(self,weights,X,y):
+    @classmethod
+    def _loss(cls,model_params,weights,X,y):
+        """Calculate the loss between y and the predictions made with X using specified parameters and weights."""
 
-        return mean_squared_error( y , self._predict(weights,X) )
+        return mean_squared_error( y , cls._predict(model_params,weights,X) )
     
-    def _predict(self,weights,X):
+    @classmethod
+    def _predict(cls,model_params,weights,X):
+        """Predict X using specified parameters and weights."""
 
-        intercept,coefs = self._unpack_weights(weights)
+        intercept,coefs = cls._unpack_weights(model_params,weights)
         predictions = []
         for vals in X:
             pred = intercept
@@ -209,7 +301,8 @@ class LinearRegression(Model):
         return predictions
 
     def predict(self,X):
-        
+        """Predict X."""
+
         assert len(X[0])==len(self.coefs), "X does not match dimensions of fitted model."
         predictions = []
         for vals in X:
@@ -220,17 +313,35 @@ class LinearRegression(Model):
         return predictions
 
     def fit(self,X,y):
+        """Fit the model to X and y."""
 
+        # Check inputs:
         X,y = _check_inputs(X,y)
         n_features = len(X[0])
         n_weights = n_features+1 if self.fit_intercept==True else n_features
-        initial_weights = [random.uniform(0,1) for _ in range(n_weights)]
-        weights = self.solver.solve(initial_weights,X,y)
+
+        # Standardize data:
+        if self.standardize:
+            X,feature_means,feature_stddevs = standardize(X,check=False,return_stats=False)
+
+        # Prepare parameters and initialize weights:
+        model_params = { 'fit_intercept' : self.fit_intercept }
+        #initial_weights = [random.uniform(0,1) for _ in range(n_weights)]
+        initial_weights = [0 for _ in range(n_weights)]
+
+        # Invoke solver (on standardized data):
+        weights = self.solver.solve(model_params,initial_weights,X,y)
 
         # Expose final values to user:
-        intercept,coefs = self._unpack_weights(weights)
+        intercept,coefs = LinearRegression._unpack_weights(model_params,weights)
         self.intercept = intercept if self.fit_intercept else None
         self.coefs = coefs
+
+    def score(self,X,y):
+        """Return the score of y and the predictions made with X."""
+
+        X,y = _check_inputs(X,y)
+        return r2_score(y,self.predict(X))    
 
 
 def gradientDescent(func, initial, rate=0.01, precision=0.00001, iteration = 2000):
